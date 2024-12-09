87760b3b412be617e438c2f571f3dc001c9d762d
durdonax <durdonasheykhadx@gmail.com>
2024-12-09 16:59:46 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let random = Math.random()*100 
+
+let multiplied = random * 2
+let divided = random / 2
+console.log(multiplied, divided);
+alert(multiplied)
+alert(divided)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
